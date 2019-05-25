c1667f2d76af3dddd0592c04c212d5cbc77d2506
Mirza Delagic <mirza.delagic@gmail.com>
2019-05-25 18:40:17 +0200

    i opt vježbanje

--- Vježbe/V1_frizerski_salon.sql
+++ Vježbe/V1_frizerski_salon.sql
@@ -37,6 +37,21 @@ djelatnica_id int not null
 
 alter table dogadjaj add foreign key (usluga_id) references usluga(id);
 alter table dogadjaj add foreign key (korisnik_id) references korisnik(id);
-alter table dogadjaj add foreign key (djelatnica) references djelatnica(id);
+alter table dogadjaj add foreign key (djelatnica_id) references djelatnica(id);
 alter table djelatnica add foreign key (osoba_id) references osoba(id);
 alter table korisnik add foreign key (osoba_id) references osoba(id);
+
+insert into osoba(ime,prezime) values('Iva','Ivić'),('Josip','Kraš');
+insert into usluga(naziv) values('Šišanje'),('Ž frizura'),('Bojanje');
+insert into djelatnica(osoba_id) values(1);
+insert into korisnik(osoba_id) values(2);
+insert into dogadjaj(vrijeme_dog,usluga_id,korisnik_id,djelatnica_id) values('2019-05-25 15:00:00',1,1,1);
+
+update usluga set cijena=12.99 where id=1;
+update djelatnica set iban='HR9325000095657746874' where id=1;
+
+insert into osoba(ime,prezime) values('Adolf','Hitler');
+select * from osoba;
+select id from osoba where ime='Adolf'; 
+delete from osoba where id=3;
+
---
+++ Vježbe/V3_azil.sql
@@ -0,0 +1,49 @@
+drop database if exists azil;
+create database azil default character set utf8;
+use azil;
+
+create table djelatnik(
+id int not null primary key auto_increment,
+ime varchar(22) not null,
+prezime varchar(22) not null,
+oib varchar(11)
+);
+
+create table sticenik(
+id int not null primary key auto_increment,
+naziv varchar(22) not null,
+vrsta varchar(22) not null,
+opis text
+);
+
+create table prostor(
+id int not null primary key auto_increment,
+naziv varchar(22) not null,
+opis text
+);
+
+create table dogadjaj(
+id int not null primary key auto_increment,
+vrijeme datetime not null,
+sticenik_id int not null,
+djelatnik_id int not null,
+prostor_id int not null
+);
+
+alter table dogadjaj add foreign key (sticenik_id) references sticenik(id);
+alter table dogadjaj add foreign key (djelatnik_id) references djelatnik(id);
+alter table dogadjaj add foreign key (prostor_id) references prostor(id);
+
+insert into djelatnik(ime,prezime) values('Dog','Lover'),('Cat','Fornicator');
+insert into sticenik(naziv,vrsta,opis) values('Lucky','ćuko','mješanac hoklice i štakora'),('Mamika','micija','stara siva mačka');
+insert into prostor(naziv,opis) values('Box1','box za male ćuke'),('Box2','box za micije');
+insert into dogadjaj(vrijeme,sticenik_id,djelatnik_id,prostor_id) values('2019-05-15',1,1,1),('2019-05-25',2,2,2);
+
+select * from djelatnik;
+select * from sticenik;
+select * from prostor;
+select * from dogadjaj;
+
+alter table sticenik add column zadnja_promjena datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+
