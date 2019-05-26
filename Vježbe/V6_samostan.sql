2dc13a5ec57e8f430623f35b063a00af760b6c26
Mirza Delagic <mirza.delagic@gmail.com>
2019-05-26 15:39:03 +0200

    samostan napravljen dodano-brisano

--- Vježbe/V6_samostan.sql
+++ Vježbe/V6_samostan.sql
@@ -4,14 +4,14 @@ use samostan;
 
 create table posao(
 id int not null primary key auto_increment,
-naziv_opis varchar(22) not null,
+naziv_opis varchar(22) not null
 );
 
 create table svecenik(
 id int not null primary key auto_increment,
 ime varchar(33) not null,
 prezime varchar(33) not null,
-svecenik_id int not null,
+svecenik_id int,
 vrijeme_kreiranja datetime not null default current_timestamp
 );
 
@@ -25,3 +25,13 @@ svecenik_id int not null
 alter table svecenik add foreign key (svecenik_id) references svecenik(id);
 alter table korektna_sljaka add foreign key (posao_id) references posao(id);
 alter table korektna_sljaka add foreign key (svecenik_id) references svecenik(id);
+
+insert into posao(naziv_opis) values('Pranje podova'),('Pranje suđa'),('Zlostavljanje maloljetnika');
+insert into svecenik(ime,prezime,svecenik_id) values('Prle','Primus',null),('Pedo','File',1);
+insert into korektna_sljaka(vrijeme,posao_id,svecenik_id) values('2019-01-01 22:00:00',3,2);
+
+select * from svecenik;
+update svecenik set prezime='Neko prezime' where id=1;
+select * from svecenik;
+alter table svecenik drop column vrijeme_kreiranja;
+select * from svecenik;
