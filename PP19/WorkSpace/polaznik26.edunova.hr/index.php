2d728827b1d0d922dab282fb959ee775e64d9701
Delaga <mirza.delagic@gmail>
2019-06-12 20:57:39 +0200

    gotovo predavanje 12-06-2019

---
+++ PP19/WorkSpace/polaznik26.edunova.hr/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello"," ";
+print "Pero";
+
+echo 'tekst ';
+echo "Dvostruki \n";
+echo 'ovo je pod jednostrukima\n';
+
+echo 'kako jednostruki \'\n';
---
+++ PP19/WorkSpace/polaznik26.edunova.hr/print_r.php
@@ -0,0 +1,5 @@
+<?php
+echo "<pre>";
+print_r($_SERVER);
+var_dump($_SERVER);
+echo "</pre>";
