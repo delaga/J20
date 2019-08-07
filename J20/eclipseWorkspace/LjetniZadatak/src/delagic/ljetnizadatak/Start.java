860062d2884a635a22f421d51e732cce01a06659
delaga <44586692+delaga@users.noreply.github.com>
2019-08-07 19:40:58 +0200

    ide ide ide

--- J20/eclipseWorkspace/LjetniZadatak/src/delagic/ljetnizadatak/Start.java
+++ J20/eclipseWorkspace/LjetniZadatak/src/delagic/ljetnizadatak/Start.java
@@ -9,14 +9,28 @@ public class Start {
 	public static void main(String[] args) {
 		Select.ispisiTablice();
 		Select.ispisiTablicu(0);
-		
 		izbornikDelanja();
-		int radnja=Integer.parseInt(JOptionPane.showInputDialog("Unesite redni broj predviðene radnje:"));
 		
+		
+		
+	}
+
+	private static void izbornikDelanja() {
+		System.out.println(">>>>>>IZBORNIK<<<<<<");
+		System.out.println("1. Brišemo?");
+		System.out.println("2. Dodajemo?");
+		System.out.println("3. Mjenjamo?");
+		System.out.println("4. Povratak na izbor tablice?");
+		System.out.println("5. IZLAZ");
+		int radnja=Integer.parseInt(JOptionPane.showInputDialog("Unesite redni broj predviðene radnje:"));
+		//int rad=izbornikDelanja();
 		switch (radnja) {
 		case 1:
 			Delete.BrisanjeIzTablice();
+			JOptionPane.showMessageDialog(null, "OBRISANO");
+			System.out.println(">>>NAKON BRISANJA<<<");
 			Select.ispisiTablicu(Select.tablica);
+			izbornikDelanja();
 			
 			
 			break;
@@ -30,20 +44,11 @@ public class Start {
 			Select.ispisiTablice();
 			break;
 		case 5:
-			
+			System.out.println();
+			System.out.println(">>>Stay beautiful and goodbye :D<<<");
 			break;
 		default:
 			break;
 		}
-	}
-
-	private static void izbornikDelanja() {
-		System.out.println(">>>>>>IZBORNIK<<<<<<");
-		System.out.println("1. Brišemo?");
-		System.out.println("2. Dodajemo?");
-		System.out.println("3. Mjenjamo?");
-		System.out.println("4. Povratak na izbor tablice?");
-		System.out.println("5. IZLAZ");
-		
 	}
 }
